5e37e7e286d5703491f32c2e6d3ca77ad9ef5b50
erikacamilleri <erika.camilleri@gmail.com>
2022-10-12 11:07:22 +0200

    Building Blocks in Java

--- lessons/src/lesson01/FirstStep.java
+++ lessons/src/lesson01/FirstStep.java
@@ -1,14 +1,25 @@
 package lesson01;
 
+/**
+ * My first class my first object is called FirstStep.
+ * I will learn about classes later, but for now I will
+ * create a class in each lesson to organise my code.
+ */
 public class FirstStep {
-    public static void main(String[] args)  {
-        // TODO: Follow your teacher and code in this space!
 
-        // TODO: An exercise for you to do as homework...
+    public static void main(String[] args)  {
+        // An example of printing out text in the console
+        System.out.println("My first step in Java!");
+
+        // An exercise we did in class...
         displayMyPersonalDetails();
     }
 
     public static void displayMyPersonalDetails() {
-        // TODO: Java In Two Semesters page 17 q 5
+        // Java In Two Semesters page 17 q 5
+        // Simple output statements to display personal info
+        System.out.println("My name is Erika Camilleri.");
+        System.out.println("My date of birth is 07/02/1991");
+        System.out.println("My hobbies include playing the piano and the guitar.");
     }
 }
---
+++ lessons/src/lesson02/README.md
@@ -0,0 +1,17 @@
+# Building Blocks
+
+## IB Learning Outcomes
+- D.1.9 Explain the need for different data types to represent data items
+- 4.3.6 Define: variable, constant, operator, object
+- 4.3.8 Use of variables, constants and operators in algorithms
+  
+## Simple Data Types in Java
+
+We need our programs to store and manipulate data so it becomes valuable information. We are surrounded by different kinds of data, therefore Javat allows
+us to easily classify various data into different types:
+
+- `int` is short for `Integer` and can store whole numbers e.g. 1, 2, -4
+- `float` is short for floating-point and can store real numbers within a specific range e.g 0.1, 0.005, 6.7
+- `double` is a real number but we use the type to store numbers of a wider range than `float` e.g. 56789321.1, 3.4, 898989898989.2221 
+- `char` is short for character so it stores alphanumeric symbols e.g. 'a', '&', '*'
+- `boolean` stores `true` or `false` this is the smallest unit of data that can be processed by any computer because it is binary

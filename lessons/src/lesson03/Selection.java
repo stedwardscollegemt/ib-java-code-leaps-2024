941b712e9bdc899ce527466cac5e1ccbed4d7408
erikacamilleri <erika.camilleri@gmail.com>
2022-10-26 13:50:30 +0200

    The if statement and test conditions

--- lessons/src/lesson03/Selection.java
+++ lessons/src/lesson03/Selection.java
@@ -10,7 +10,14 @@ public class Selection {
         System.out.println("Please enter an age between 5 and 100 inclusive: ");
         age = Keyboard.readInt();
 
-        // TODO: Milestone activities to show you reached a certain age
-
+        // Milestone activities to show you reached a certain age
+        if (age >= 60) {
+            // This code will run if and only if age is greater or equal to 60
+            System.out.println("Enjoy your golden years!");
+        }
+        if (age < 60 && age >= 21) {
+            // This code will run if age is less than 60 AND age is greater or equal to 21
+            System.out.println("You can drink and drive (not at the same time)!");
+        }
     }
 }

dc1b836b59b8159a8a7fe8e4ebc9e7eb1c2416f4
erikacamilleri <erika.camilleri@gmail.com>
2022-11-05 14:38:39 +0100

    Topic 4 Activity Stack Perfect Cheeseburger

---
+++ lessons/src/topic04learningaids/StackPerfectCheeseburger.java
@@ -0,0 +1,31 @@
+package topic04learningaids;
+
+/**
+ * Topic 4.1.1 Thinking Procedurally
+ */
+public class StackPerfectCheeseburger {
+
+    static String perfectBurger;
+
+    public static void main(String[] args) {
+        String[] ing = {"TB", "S1", "O", "P", "CH", "PA", "T", "L", "S2", "BB"};
+
+        int currentIng = 10;
+
+        while(currentIng > 0) {
+            stack(ing[currentIng - 1]);
+            currentIng = currentIng - 1; 
+        }
+
+        System.out.println("Your dinner is served. Buon apetit!\n");
+        System.out.println(perfectBurger);
+    }
+
+    public static void stack(String ing) {
+        if (perfectBurger == null) {
+            perfectBurger = ing;
+        } else {
+            perfectBurger = ing + "\n" + perfectBurger; 
+        }
+    }
+}

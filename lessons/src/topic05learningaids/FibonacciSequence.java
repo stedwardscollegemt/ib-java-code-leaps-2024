e4417f7aadbf0cb82c42553ec76fe718cf3e2768
erikacamilleri <erika.camilleri@gmail.com>
2022-11-30 09:03:09 +0100

    I forgot to push this, sorry!

--- lessons/src/topic05learningaids/FibonacciSequence.java
+++ lessons/src/topic05learningaids/FibonacciSequence.java
@@ -1,21 +1,31 @@
 package topic05learningaids;
 
+import helpers.Keyboard;
+
 /**
  * Thinking Recursively
+ * P - Predict. Write or draw what you think the program will output.
+ * R - Run. Run the code and compare the actual result with your prediction.
+ * I - Investigate. Work with someone to fill in the // with explainers of what the code is doing.
+ * M - Modify. Make one small change, does not have to be spectacular.
+ * M - Make. Create a new class to print out the Perfect Square Series from first principles. ==> (https://brilliant.org/courses/math-fundamentals/intro-to-math-fundamentals/)
  */
 public class FibonacciSequence {
     
     public static void main(String[] args) {
-        // TODO: call the printFibonacciSeries and pass in 6 and 0
-    }
+       System.out.println("*** Fibonacci Sequence ***");
+       System.out.println("**************************");
 
-    public static void printFibonacciSeries(int n, int step) {
-        System.out.println(fibonacci(step) + " ");
-
-        if (step < n) {
-            step = step + 1;
-            printFibonacciSeries(n, step);
-        }
+       // 
+       System.out.println("Please enter a number between 0 and 6:");
+       int fiboN = Keyboard.readInt();
+       System.out.println("--------------------------------------");
+    
+       // 
+       for (int i = 1; i <= fiboN; i++) {
+            System.out.print(fibonacci(i) + ", ");
+       }
+       System.out.println("");
     }
 
     public static int fibonacci(int n) {

deb7141fa334c44312ce9448bebccba8292e82cc
erikacamilleri <erika.camilleri@gmail.com>
2022-12-07 10:38:17 +0100

    Largest Number using a while loop

--- lessons/src/lesson04/LargestNumberWhileLoop.java
+++ lessons/src/lesson04/LargestNumberWhileLoop.java
@@ -4,27 +4,42 @@ import helpers.Keyboard;
 
 public class LargestNumberWhileLoop {
     public static void main(String[] args) {
-        int largestNumber, n1, n2, n3;
+        int n1, n2;
 
-        // Step 1. Ask for the user to input 3 numbers, n1, n2, n3
-        System.out.println("Please enter 3 numbers:");
+        // Step 1. Ask for the user to input n1
+        System.out.println("Please enter a number:");
         n1 = Keyboard.readInt();
-        n2 = Keyboard.readInt();
-        n3 = Keyboard.readInt();
 
-        // Step 2. If n1 > n2 then largestNumber = n1
-        if (n1 > n2) {
-            largestNumber = n1;
-        } else {
-            largestNumber = n2;
-        }
-
-        // Step 3. If n3 > largestNumber then largestNumber = n3 
-        if (n3 > largestNumber) {
-            largestNumber = n3;
+        // 
+        int counter = 0;
+        while (counter < 5) {
+            // Ask for the user to input n2
+            System.out.println("Please enter another number:");
+            n2 = Keyboard.readInt();
+            
+            int result = compare(n1, n2);
+            if (result == 1) {
+                n1 = n2;
+            }
+            
+            counter = counter + 1; // we made a comparison
         }
 
         // Step 4. Display the largest number
-        System.out.println(largestNumber);
+        System.out.println("Largest number is " + n1);
+    }
+
+    public static int compare(int n1, int n2) {
+
+        if(n1 == n2) {
+            return 0; // equal
+        }
+        
+        if (n1 < n2) {
+            return 1; // n2 is larger
+        }
+        
+        // this means that n1 is larger
+        return 2;
     }
 }

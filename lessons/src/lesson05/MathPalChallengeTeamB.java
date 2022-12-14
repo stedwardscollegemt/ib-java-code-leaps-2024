b63aebe461ed6f43cbc24cc498c3945d8b2aba11
erikacamilleri <erika.camilleri@gmail.com>
2022-12-14 11:16:56 +0100

    Revision on creating methods and calling them.

--- lessons/src/lesson05/MathPalChallengeTeamB.java
+++ lessons/src/lesson05/MathPalChallengeTeamB.java
@@ -28,32 +28,81 @@ public class MathPalChallengeTeamB {
             if (function.equals("Add")) {
                 output = calc(n1, n2, '+');
                 System.out.println(n1 + " * " + n2 + " = " + output);
-                explainer();
-            } else if (function.equals("Multiply"))
+                explainer(n1, n2, '+');
+            } else if (function.equals("Multiply")) {
                 output = calc(n1, n2 , '*');
                 System.out.println(n1 + " * " + n2 + " = " + output);
-                explainer();
-            } // TODO: continue if statements for the rest of the functions...
-        }
+                explainer(n1, n2, '*');
+            } else if (function.equals("Divide")) {
+                output = calc(n1, n2 , '/');
+                System.out.println(n1 + " / " + n2 + " = " + output);
+                explainer(n1, n2, '/');
+            } else if (function.equals("Subtract")) {
+                output = calc(n1, n2 , '-');
+                System.out.println(n1 + " - " + n2 + " = " + output);
+                explainer(n1, n2, '-');
+            } else {
 
-        /**
-         * A method that performs a calculation of two numbers based on an operator
-         * @param n1
-         * @param n2
-         * @param operator
-         * @return
-         */
-        public static int calc(int n1, int n2, char operator) {
-            // TODO: use a switch case statement to switch between operator
-            // in each case, return the appropriate result e.g., n1 + n2
-            return 5; 
-        
+            }
         }
+    }
 
-        // TODO: Have separate explainer() that accept appropriate parameters to keep the main() clean
-        // and easy to read e.g., addExplainer(int n1, int n2) { ... }. The methods do not return anything (void)
-        // and should output the explainer using the inputs
-        public static void explainer() {
-            System.out.println("output explainer");
+    /**
+     * A method that performs a calculation of two numbers based on an operator
+     * @param n1
+    * @param n2
+    * @param operator
+    * @return
+    */
+    public static int calc(int n1, int n2, char operator) {
+        int result = -1;
+        switch(operator) {
+            case '+':
+                result = n1 + n2;
+                break;
+            case '-':
+                result = n1 - n2;
+                break;
+            case '/':
+                result = (int) (n1 / n2); // 
+                break;
+            case '*':
+                result = n1 * n2;
+                break;
+            default:
+                System.out.println("ERR: I cannot recognise this operator: " + operator);
+            }   
+        return result;
+    }
+
+    public static void explainer(int n1, int n2, char operator) {
+        switch(operator) {
+            case '+':
+                System.out.println("Start counting from n1: " + n1);
+                System.out.println("Keep counting for n2 (" + n2 + ") times.");
+                System.out.println("Example...");
+                for (int count = n1; count <= (count + n2); count++) {
+                System.out.println(count);
+                }
+                break;
+            case '-':
+                System.out.println("Start counting from n1: " + n1);
+                System.out.println("Keep counting for n2 (" + n2 + ") times but backwards.");
+                System.out.println("Example...");
+                for (int count = n1; count >= (count - n2); count--) {
+                    System.out.println(count);
+                }
+                break;
+            case '/':
+                System.out.println("Imagine you are sharing toys equally among children.");
+                System.out.println("If you have 5 toys but 2 children... each child will get...");
+                System.out.println("Each child gets 2 whole toys, and a broken toy...");
+                break;
+            case '*':
+                System.out.println("You are on your own kid, do this by heart!");
+                break;
+            default:
+                System.out.println("ERR: I cannot recognise this operator: " + operator);
         }
+    }
 }

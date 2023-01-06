f1131e2ab46a8ab70bd8b35b9549372066f3a4c0
erikacamilleri <erika.camilleri@gmail.com>
2023-01-06 18:18:42 +0100

    Learning about the nature of arrays

--- lessons/src/lesson06/DemoArrayHelper.java
+++ lessons/src/lesson06/DemoArrayHelper.java
@@ -3,7 +3,23 @@ package lesson06;
 import helpers.ArrayHelper;
 
 public class DemoArrayHelper {
-    public static void main(String[] args) {
+    
+    static void main(String[] args) {
+        // declare one variable for one number
+        // assign myNumber to 5
+        int myNumber = 5;
 
+        // declare an array for 100 numbers
+        // assign memory locations
+        int[] myNumbers = new int[100];
+
+        // using array helpers
+        ArrayHelper.fill(myNumbers, myNumber);
+
+        boolean found = ArrayHelper.contains(myNumbers, myNumber);
+        System.out.println(found); // we expect true
+        
+        boolean notFound = ArrayHelper.contains(myNumbers, 6);
+        System.out.println(notFound); // we expect false
     }
 }

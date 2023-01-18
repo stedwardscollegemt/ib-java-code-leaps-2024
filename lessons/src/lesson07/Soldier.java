d55464b208ae54ab16dc17098a5ddade54e98b3b
erikacamilleri <erika.camilleri@gmail.com>
2023-01-18 10:24:14 +0100

    Objects as a programming concept: Soldier.java

--- lessons/src/lesson07/Soldier.java
+++ lessons/src/lesson07/Soldier.java
@@ -1,5 +1,46 @@
 package lesson07;
 
+/**
+ * 
+ */
 public class Soldier {
+    /**
+     * ------------- Attributes -------------------------------------
+     * These are all the properties that describe the state.
+     */
+    int id;
     
+    int health;
+
+    String name;
+
+    String hungerLevel = "Hamborgor";
+
+    boolean isHungry;
+
+    /**
+     *  ------------- Coming Soon -----------------------------------
+    */
+
+    /**
+     * ------------- Behavioral Methods -----------------------------
+     * The functions the object can use to update the state.
+    */
+    public void damage() {
+        health = health - 1;
+    }
+
+    public void eat() {
+        if (isHungry) {
+            switch(hungerLevel) {
+                case "Hamborgor": {
+                    // todo give him 10 hamburgers
+                }
+                case "Hamburger": {
+                    // todo give him 0 hamburger
+                }
+            }
+            isHungry = false;
+        }
+    }
 }

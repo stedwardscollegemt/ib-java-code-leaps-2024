144c31dc7360990e1d4edc9d355d15b3ae6453e8
erikacamilleri <erika.camilleri@gmail.com>
2023-01-18 11:41:15 +0100

    Practicing with String object reference

--- lessons/src/lesson07/WorkingWithStrings.java
+++ lessons/src/lesson07/WorkingWithStrings.java
@@ -1,5 +1,28 @@
 package lesson07;
 
+import helpers.Keyboard;
+
 public class WorkingWithStrings {
-    
+    public static void main(String[] args) {
+        
+        String myString = new String();
+        boolean endWith = myString.endsWith("s");
+
+        // Declaration and assignment of strings
+        String paymentTypeCard = new String("Card");
+        String paymentTypeCash = "Cash"; // you will use this method most
+
+        // Ask for the payment type as input
+        System.out.println("Enter your payment type (Cash/Card)");
+        String userPaymentType = Keyboard.readString();
+
+        // Compare userPaymentType with existing paymentTypes
+        if (userPaymentType.equals(paymentTypeCard)) { // we cannot use == because Java will check for the memory location, not compare state
+            System.out.println("You are paying by card.");
+        }
+
+        if (userPaymentType.equals(paymentTypeCash)) {
+            System.out.println("You are paying by cash.");
+        }
+    }
 }

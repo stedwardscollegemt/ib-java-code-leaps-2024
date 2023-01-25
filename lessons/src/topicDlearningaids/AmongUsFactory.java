71649202f9e93a66b448faa3638c5b774814e80e
erikacamilleri <erika.camilleri@gmail.com>
2023-01-25 11:10:07 +0100

    Trying to debug creation of objects

--- lessons/src/topicDlearningaids/AmongUsFactory.java
+++ lessons/src/topicDlearningaids/AmongUsFactory.java
@@ -6,7 +6,15 @@ public class AmongUsFactory {
         // Example of using the "plain cookie cutter" constructor
         // What AmongUs colour do we get?
         AmongUs randomAstronaut = new AmongUs();
+        
+        AmongUs[] allCrew = new AmongUs[18];
          
         // TODO: Create an instances of the AmongUs in every colour
+        for(int c = 0; c < AmongUs.WARDROBE.length; c++) {
+            // create an instance of AmongUs in a colour
+            AmongUs astro = new AmongUs(AmongUs.WARDROBE[c]);
+            // store it in allCrew
+            allCrew[c] = astro;
+        }
     }
 }

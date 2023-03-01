e6b7d911c3bbc9d78f06b9deb60eb0ebe99afc27
erikacamilleri <erika.camilleri@gmail.com>
2023-03-01 10:02:58 +0100

    Lesson Stack Pancake Example Updates

---
+++ lessons/src/topic05learningaids/stacks/PancakeMain.java
@@ -0,0 +1,8 @@
+package topic05learningaids.stacks;
+
+public class PancakeMain {
+    public static void main(String[] args) {
+        // instansiate a PancakeStack
+        PancakeStack myBreakfast = new PancakeStack("Butter and Maple Syrup");
+    }
+}
--- lessons/src/topic05learningaids/stacks/PancakeStack.java
+++ lessons/src/topic05learningaids/stacks/PancakeStack.java
@@ -5,30 +5,46 @@ public class PancakeStack {
     /**
      * --- Attributes (State) -----------------------------
      */
-
     int size; // this is a stack attribute
 
     Pancake[] stack = new Pancake[5];
 
     String topping; // this is a pancake attribute
 
+    public PancakeStack(String topping) {
+        this.topping = topping;
+        this.size = 0;
+    }
+
     /**
      * --- Methods (Behaviour) -----------------------------
      */
     public void push() {
-        // create the pancake
-        Pancake p = new Pancake();
-        int pos = stack.length - size;
-        stack[pos] = p;
-        size++;
+        if (!isFull()) {
+            // create the pancake
+            Pancake p = new Pancake();
+            int pos = stack.length - size;
+            stack[pos] = p;
+            size++;
+        }
     }
 
     public Pancake pop() {
-        int pos = stack.length - size;
-        Pancake p = stack[pos];
-        stack[pos] = null;
-        size--;
-        return p;
-    }
+        if(!isEmpty()) {
+            int pos = stack.length - size;
+            Pancake p = stack[pos];
+            stack[pos] = null;
+            size--;
+            return p;
+        }
+        return null;
+    } 
     
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == stack.length;
+    }
 }

e7c119da0fbafe884a1acbb3c7440c53e7ac4b8a
erikacamilleri <erika.camilleri@gmail.com>
2023-03-01 09:44:33 +0100

    Linear Search 2D array

--- lessons/src/topic04learningaids/LinearSearch.java
+++ lessons/src/topic04learningaids/LinearSearch.java
@@ -22,11 +22,46 @@ public class LinearSearch {
         return -1; // not found because -1 is not an index that exists
     }
 
-    public static int[][] search2D(int[][] numarr, int target) {
-        // TODO
-    }
-
     public static int search(String[] strarr, String target) {
         return -1;
+    }
+
+    /**
+     * HL Extension Only
+     * 
+     * @param numarr
+     * @param target
+     * @return
+     */
+    public static int[] search2D(int[][] numarr, int target) {
+        int[] position = {-1, -1};
+        for(int i = 0; i < numarr.length; i++) {
+            for(int j = 0; j < numarr[0].length; j++) {
+                if (numarr[i][j] == target) {
+                    position[0] = i;
+                    position[1] = j;
+                }
+            }
+        }
+        return position;
+    }
+
+    /**
+     * HL Extension Only
+     * @param strarr
+     * @param target
+     * @return
+     */
+    public static int[] search2D(String[][] strarr, String target) {
+        int[] position = {-1, -1};
+        for(int i = 0; i < strarr.length; i++) {
+            for(int j = 0; j < strarr[0].length; j++) {
+                if (strarr[i][j].equals(target)) {
+                    position[0] = i;
+                    position[1] = j;
+                }
+            }
+        }
+        return position;
     }
 }

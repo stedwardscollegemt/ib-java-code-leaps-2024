2d43f640c7b39c2673868c5356fb607450555b1e
erikacamilleri <erika.camilleri@gmail.com>
2023-05-08 14:40:06 +0200

    Added findPositionOf item in CollectionHlper

--- lessons/src/helpers/CollectionHelper.java
+++ lessons/src/helpers/CollectionHelper.java
@@ -8,8 +8,15 @@ import java.util.Iterator;
  */
 public class CollectionHelper {
     
+    /**
+     * Practice the running total
+     * 
+     * @param col
+     * @return
+     */
     public static int sum(Collection<Integer> col) {
         Integer sum = 0;
+        
         // get the iterator from the collection
         Iterator<Integer> iterator = col.iterator();
 
@@ -19,5 +26,27 @@ public class CollectionHelper {
         }
         
         return (int) sum;
+    }
+
+    /**
+     * Practice the linear search using collections
+     * @param col
+     * @param target
+     * @return
+     */
+    public static int findPositionOf(Collection<Integer> col, int target) {
+        // get the iterator from the collection
+        Iterator<Integer> iterator = col.iterator();
+        
+        int position = -1;
+        while(iterator.hasNext()) {
+            Integer nextInt = iterator.next();
+            position = position + 1;
+            if (nextInt == target) {
+                return position;
+            }
+        }
+
+        return -1; // if we reach here it means we did not find
     }
 }

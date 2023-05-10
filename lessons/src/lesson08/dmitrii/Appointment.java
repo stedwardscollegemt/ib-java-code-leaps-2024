a66c66e467a7c4babc3792c0e25763dc2ded2fde
erikacamilleri <erika.camilleri@gmail.com>
2023-05-10 11:41:22 +0200

    Deconstruction of objects from UML

---
+++ lessons/src/lesson08/dmitrii/Appointment.java
@@ -0,0 +1,37 @@
+package lesson08.dmitrii;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class Appointment {
+    /**
+     * --- Attributes ----------------------------
+     */
+    int appointmentId;
+
+    String title;
+
+    String type;
+
+    ArrayList<Guest> guests;
+
+    LocalDateTime date;
+
+    Location location;
+
+    Description description;
+
+    Calendar calendar;
+
+    ArrayList<Notification> notifications;
+
+    /**
+     * --- Constructor ---------------------------
+     */
+
+
+
+    /**
+     * --- Behaviour ----------------------------
+     */
+}

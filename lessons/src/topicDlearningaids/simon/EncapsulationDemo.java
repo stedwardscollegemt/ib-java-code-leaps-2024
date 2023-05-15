a7c568b9fd612ab42d40854d47a320a0e5e2bf72
erikacamilleri <erika.camilleri@gmail.com>
2023-05-15 09:17:26 +0200

    Encapsulation lesson WIP

---
+++ lessons/src/topicDlearningaids/simon/EncapsulationDemo.java
@@ -0,0 +1,12 @@
+package topicDlearningaids.simon;
+
+/**
+ * This is just a class with the main() to explain what "information hiding" truly means.
+ */
+public class EncapsulationDemo {
+    public static void main(String[] args) {
+        
+        // todo: Create an instance of a student
+        
+    }
+}
--- lessons/src/topicDlearningaids/simon/Student.java
+++ lessons/src/topicDlearningaids/simon/Student.java
@@ -1,9 +1,21 @@
 package topicDlearningaids.simon;
 
+import java.util.Date;
+
+/**
+ * Encapsulation: public, protected, private
+ */
 public class Student {
     /**
      * --- Attributes --------------------------
      */
+    public String name;
+
+    public String surname;
+
+    public Date dob;
+
+    public int year;
 
     /**
      * --- Constructor --------------------------

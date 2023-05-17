0f1dc3c7ea6963bc2427b3788cf81462302b4de0
erikacamilleri <erika.camilleri@gmail.com>
2023-05-17 09:57:59 +0200

    Using standard library collections in JETs

---
+++ lessons/src/topicDlearningaids/hl/DemoCollectionLibraryJETS.java
@@ -0,0 +1,12 @@
+package topicDlearningaids.hl;
+
+import java.util.ArrayList;
+
+public class DemoCollectionLibraryJETS {
+    
+    public static void main(String[] args) {
+        ArrayList<Animal> myAnimalsList = new ArrayList<Animal>();
+        
+        System.out.println(myAnimalsList.isEmpty());
+    }
+}

44613f3bcec7d93273f6a06b80dee6cfbef39ea6
erikacamilleri <erika.camilleri@gmail.com>
2023-05-17 09:54:09 +0200

    Unit testing for AnimalCollection

--- lessons/src/topicDlearningaids/hl/AnimalCollectionDemo.java
+++ lessons/src/topicDlearningaids/hl/AnimalCollectionDemo.java
@@ -3,7 +3,24 @@ package topicDlearningaids.hl;
 public class AnimalCollectionDemo {
     
     public static void main(String[] args) {
+
+         
+          
+    }
+
+    public static void testIsEmptyTrue() {
         
+    }
+
+    public static void testIsEmptyFalse() {
+        Animal myAnimal = new Animal("Maltese Dog", "Malta", null, null, true);
+
+        AnimalCollection myAnimalCollection = new AnimalCollection(myAnimal);
+
+        System.out.println(myAnimalCollection.isEmpty());
+    }
+
+    public static void testOne() {
         // target animal to search for
         Animal targetAnimal = new Animal("Persian Cat", "Egypt", null, null, false);
 
@@ -11,7 +28,7 @@ public class AnimalCollectionDemo {
         boolean isFound = false;
 
         // create an instance of my Animal Collection
-        AnimalCollection myAnimals = new AnimalCollection();
+        AnimalCollection myAnimals = new AnimalCollection(null);
 
         // reset to the head
         myAnimals.resetNext();

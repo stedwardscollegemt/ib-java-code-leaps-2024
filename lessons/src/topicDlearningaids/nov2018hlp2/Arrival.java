b16c67d50fc68346a4ced1afd46d0e965d796560
erikacamilleri <erika.camilleri@gmail.com>
2023-05-31 09:38:30 +0200

    Added Arrival.java

--- lessons/src/topicDlearningaids/nov2018hlp2/Arrival.java
+++ lessons/src/topicDlearningaids/nov2018hlp2/Arrival.java
@@ -2,4 +2,26 @@ package topicDlearningaids.nov2018hlp2;
 
 public class Arrival {
     
+    // -------- Attributes (Data) ----------------------------------------------------
+
+    // The use of the private keyword is data-hiding which is related to *encapsulation*
+    private Flight myFlight;
+    private String sta; // Scheduled Time of Arrival hh:mm e.g. 01:10
+    private int runway;
+    private String gate;
+    private int delay;
+    private boolean landed;
+
+    // -------- Methods ---------------------------------------------------------------
+
+    public int compareWith(String flightId) {
+        if(this.myFlight.getId().equals(flightId)) {
+            return 0;
+        }
+        return 1;
+    }
+
+    public int compareWith(Arrival anotherArrival) {
+
+    }
 }
